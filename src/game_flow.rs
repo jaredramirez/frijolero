79145ce8fa0adaa05a3d904bbfcf3269c915b810
Jared Ramirez <jaredpramirez@proton.me>
2025-03-02 16:19:10 -0800

    Add world and level respawning

--- src/game_flow.rs
+++ src/game_flow.rs
@@ -38,14 +38,15 @@ pub fn update_level_selection(
     ldtk_project_assets: Res<Assets<LdtkProject>>,
 ) {
     for (level_iid, level_transform) in &level_query {
+        // Load the project & level
         let ldtk_project = ldtk_project_assets
             .get(ldtk_projects.single())
             .expect("Project should be loaded if level has spawned");
-
         let level = ldtk_project
             .get_raw_level_by_iid(&level_iid.to_string())
             .expect("Spawned level should exist in LDtk project");
 
+        // Get the bounds of the level
         let level_bounds = Rect {
             min: Vec2::new(level_transform.translation.x, level_transform.translation.y),
             max: Vec2::new(
@@ -54,6 +55,18 @@ pub fn update_level_selection(
             ),
         };
 
+        // Check if the level is an iid
+        let is_iid = match *level_selection {
+            LevelSelection::Iid(_) => true,
+            _ => false,
+        };
+        // Ensure that the level_selection is always an iid. This is needed by
+        // the respawn logic
+        if level_selection.is_match(&LevelIndices::default(), level) && !is_iid {
+            *level_selection = LevelSelection::iid(level.iid.clone());
+        }
+
+        // Check if the player is in bounds of another level
         for player_transform in &player_query {
             if player_transform.translation.x < level_bounds.max.x
                 && player_transform.translation.x > level_bounds.min.x
@@ -67,6 +80,7 @@ pub fn update_level_selection(
     }
 }
 
+/// Respawns the entire world for the currently selected ldtk gamefile
 fn respawn_world(
     mut commands: Commands,
     ldtk_projects: Query<Entity, With<LdtkProjectHandle>>,
@@ -74,7 +88,7 @@ fn respawn_world(
     game_file: Res<GameFile>,
     asset_server: Res<AssetServer>,
 ) {
-    if input.just_pressed(KeyCode::KeyR) {
+    if input.just_pressed(KeyCode::KeyG) {
         commands.entity(ldtk_projects.single()).despawn_recursive();
 
         let ldtk_handle = asset_server
@@ -87,12 +101,108 @@ fn respawn_world(
     }
 }
 
+/// Respawn the current level and move the player to that level's respawn point.
+/// If the level has not respawn point, do nothing
+fn respawn_level(
+    mut commands: Commands,
+    ldtk_projects: Query<&LdtkProjectHandle>,
+    ldtk_project_assets: Res<Assets<LdtkProject>>,
+    input: Res<ButtonInput<KeyCode>>,
+
+    level_selection: Res<LevelSelection>,
+    levels: Query<(Entity, &LevelIid)>,
+    player_respawns: Query<(&PlayerRespawn, &Transform), Without<Player>>,
+    mut players: Query<&mut Transform, (With<Player>, Without<PlayerRespawn>)>,
+) {
+    if input.just_pressed(KeyCode::KeyR) {
+        // First, we have to find the level entity for the selected level
+        if let LevelSelection::Iid(level_selection_iid) = level_selection.as_ref() {
+            for (level_ent, level_iid) in levels.iter() {
+                if level_iid == level_selection_iid {
+                    // Then, get some level metadata
+                    let ldtk_project = ldtk_project_assets
+                        .get(ldtk_projects.single())
+                        .expect("Project should be loaded during respawning");
+                    let level = ldtk_project
+                        .get_raw_level_by_iid(level_iid.get())
+                        .expect("Level should be loaded during respawning");
+
+                    // Find the correct respawn point for the currently level
+                    // selected level
+                    let opt_respawn_point = player_respawns.iter().find_map(
+                        |(player_respawn, player_respawn_transform)| {
+                            if player_respawn.level_uid == level.uid {
+                                Some(Vec2::new(
+                                    player_respawn_transform.translation.x,
+                                    player_respawn_transform.translation.y,
+                                ))
+                            } else {
+                                None
+                            }
+                        },
+                    );
+
+                    // Then, respawn the level and move the player to the
+                    // respawn point
+                    if let Some(respawn_point) = opt_respawn_point {
+                        commands.entity(level_ent).insert(Respawn);
+                        for mut player_transform in players.iter_mut() {
+                            player_transform.translation.x = respawn_point.x;
+                            player_transform.translation.y = respawn_point.y;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Player respawn
+
+/// tag for invisible location where the player should respawn
+#[derive(Clone, Default, Debug, Component)]
+pub struct PlayerRespawn {
+    level_uid: i32,
+}
+
+/// The bundle of player respawn MUST be wordly. When this bundled is added,
+/// we use the translation position provided by the auto-added `Transform`
+/// component  to teleport the player to the respawn point. Becuaes the player
+/// is wordly, this component must be, because level-based and world-based
+/// components have different coordinate systems.
+#[derive(Clone, Default, Debug, Bundle)]
+pub struct PlayerRespawnBundle {
+    player_respawn: PlayerRespawn,
+    worldly: Worldly,
+}
+
+impl LdtkEntity for PlayerRespawnBundle {
+    fn bundle_entity(
+        entity_instance: &EntityInstance,
+        layer_instance: &LayerInstance,
+        _: Option<&Handle<Image>>,
+        _: Option<&TilesetDefinition>,
+        _: &AssetServer,
+        _: &mut Assets<TextureAtlasLayout>,
+    ) -> Self {
+        Self {
+            player_respawn: PlayerRespawn {
+                level_uid: layer_instance.level_id,
+            },
+            worldly: Worldly::from_entity_info(entity_instance),
+        }
+    }
+}
+
+// Plugin Wireup
+
 pub struct GameFlowPlugin;
 
 impl Plugin for GameFlowPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup)
+            .register_ldtk_entity::<PlayerRespawnBundle>("Player_Respawn")
             .add_systems(Update, update_level_selection)
-            .add_systems(Update, respawn_world);
+            .add_systems(Update, (respawn_world, respawn_level));
     }
 }
